2bce319eb86ba37d5e911f4a675d5907b60745de
Anton Zhiliuk <crashtown.pal@gmail.com>
2017-07-24 11:39:04 +0300

    edit about

--- src/cljs/forest_community_blog/components/about_page.cljs
+++ src/cljs/forest_community_blog/components/about_page.cljs
@@ -5,11 +5,9 @@
    [:div.row
     [:div.col-md-8.col-md-offset-2
      [:h1 "About"]
-     [:p (str "Hello, my name is Anton Žiliuk. "
+     [:p (str "\tHello, my name is Anton Žiliuk.\n"
               "I am systems engineer who interested in declarative programming. "
               "At my free time I compose electronic music and techno. "
-              "This page contains my thoughts, experiments, records. "
-              "My favorite styles are Ambient Techno, Hardtek/tribe and Dub. "
-              "Papabless! And any feedback is appreciated! ")]
-
+              "This page contains my thoughts, experiments, records.")]
+     [:p "Any feedback is appreciated!"]
      [:p "crashtown.pal@gmail.com"]]]])
