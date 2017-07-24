cf73f3b923423dba13b95b31ecbe27842ebc049e
Anton Zhiliuk <crashtown.pal@gmail.com>
2017-07-24 11:06:08 +0300

    use audio tags

---
+++ src/cljs/forest_community_blog/components/music_page.cljs
@@ -0,0 +1,38 @@
+(ns cljs.forest-community-blog.components.music-page
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.core.async :refer [<!]]
+            [cljs.forest-community-blog.cfg :refer [api-uri]]
+            [cljs.forest-community-blog.entities :refer [raw->Track]]
+            [cljs.forest-community-blog.state :refer [app-state]]
+            [cljs-http.client :as http]))
+
+
+;; EFFECTs
+(defn fetch-tracks! []
+  (go (let [response (<! (http/get (str api-uri "/tracks")
+                                   {:with-credentials? false
+                                    :headers {"content-type" "application/json"}}))
+            raw-tracks (:body response)
+            tracks (map raw->Track raw-tracks)]
+        (swap! app-state assoc :tracks tracks))))
+
+;; COMPONENTs
+(defn track-entry [track]
+  [:div.row
+   [:p.vertical-align
+    (:title track)
+    [:audio {:controls true
+             :src (:uri track)}]]])
+
+(defn tracklist [tracks]
+  [:div.col-md-8.col-md-offset-2
+   (for [track tracks]
+     ^{:key (:id track)} [track-entry track])])
+
+(defn music []
+  (fetch-tracks!)
+  (fn []
+    (let [tracks (@app-state :tracks)]
+      [:div.container
+       [:div.row
+        [tracklist tracks]]])))
